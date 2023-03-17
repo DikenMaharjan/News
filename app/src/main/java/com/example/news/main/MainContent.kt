041ab1959416d3887aea532747fd4d62b4feab40
Diken <dikenmaharjan10@gmail.com>
2023-03-17 19:12:38 +0545

    refactor: move MainActivity into main package

---
+++ app/src/main/java/com/example/news/main/MainContent.kt
@@ -0,0 +1,47 @@
+package com.example.news.main
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.news.feature_detail.DetailScreen
+import com.example.news.feature_list.ListScreen
+import com.example.news.navigation.routes.DetailRoute
+import com.example.news.navigation.routes.ListRoute
+import com.example.news.navigation.screen
+
+
+@Composable
+fun MainContent(
+    modifier: Modifier = Modifier
+) {
+    val navController = rememberNavController()
+
+    Surface(
+        modifier = modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        NavHost(
+            navController = navController,
+            startDestination = ListRoute.route
+        ) {
+            screen(
+                screenRoute = ListRoute
+            ) {
+                ListScreen(
+                    navigateToDetail = {
+                        navController.navigate(DetailRoute.route)
+                    }
+                )
+            }
+            screen(
+                screenRoute = DetailRoute
+            ) {
+                DetailScreen()
+            }
+        }
+    }
+}
