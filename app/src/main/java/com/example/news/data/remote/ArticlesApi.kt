6d85bdd93933bd3b36eebc0699677ec61a97fcaf
Diken <dikenmaharjan10@gmail.com>
2023-03-18 09:58:55 +0545

    fix: get top-headlines instead of everything from api endpoint

--- app/src/main/java/com/example/news/data/remote/ArticlesApi.kt
+++ app/src/main/java/com/example/news/data/remote/ArticlesApi.kt
@@ -3,11 +3,16 @@ package com.example.news.data.remote
 import com.example.news.data.models.response.fetchnewsresponse.FetchNewsResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 
 interface ArticlesApi {
 
-    @GET("/v2/everything")
-    suspend fun getNews(): Response<FetchNewsResponse>
+    @GET("/v2/top-headlines")
+    suspend fun getNews(
+        @Query("country") country: String = "us",
+        @Query("page") page: Int = 1,
+        @Query("pageSize") pageSize: Int = 20
+    ): Response<FetchNewsResponse>
 
 }
