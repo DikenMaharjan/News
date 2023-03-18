4505f97359e803a7d52c46b12c6244c92ff95788
Diken <dikenmaharjan10@gmail.com>
2023-03-18 09:45:15 +0545

    feat: add http logging interceptor

--- app/src/main/java/com/example/news/di/NetworkModule.kt
+++ app/src/main/java/com/example/news/di/NetworkModule.kt
@@ -6,6 +6,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -35,10 +36,14 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun providesHttpClient() = OkHttpClient.Builder().addInterceptor { chain ->
-        val request = chain.request().newBuilder().addHeader(
-            "x-api-key", API_KEY
-        ).build()
-        chain.proceed(request)
-    }.build()
+    fun providesHttpClient() =
+        OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val request = chain.request().newBuilder().addHeader(
+                    "x-api-key", API_KEY
+                ).build()
+                chain.proceed(request)
+            }
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
 }
