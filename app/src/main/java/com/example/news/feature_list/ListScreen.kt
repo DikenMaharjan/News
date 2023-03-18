9a8a4e695a6915d737f6eeaa2fadc0d94197e284
Diken <dikenmaharjan10@gmail.com>
2023-03-18 22:36:11 +0545

    feat: add functionality to select category

--- app/src/main/java/com/example/news/feature_list/ListScreen.kt
+++ app/src/main/java/com/example/news/feature_list/ListScreen.kt
@@ -1,12 +1,14 @@
 package com.example.news.feature_list
 
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -19,6 +21,7 @@ import com.example.news.feature_list.composables.TopArticleItem
 
 private const val TAG = "ListScreen"
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListScreen(
     modifier: Modifier = Modifier,
@@ -52,6 +55,32 @@ fun ListScreen(
                 }
             }
         }
-    }
 
+        item {
+            Text(
+                text = "Categories",
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(top = 48.dp)
+            )
+        }
+
+        item {
+            Row(
+                modifier = Modifier
+                    .padding(vertical = 8.dp)
+                    .horizontalScroll(rememberScrollState())
+            ) {
+                ListScreenViewModel.AvailableCategories.values().forEach {
+                    FilterChip(
+                        modifier = Modifier.padding(end = 12.dp), onClick = {
+                            viewModel.selectCategory(it)
+                        }, label = {
+                            Text(text = it.title)
+                        }, selected = viewModel.selectedCategory == it
+                    )
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/news/feature_list/ListScreenViewModel.kt
+++ app/src/main/java/com/example/news/feature_list/ListScreenViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.news.feature_list
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
@@ -14,7 +17,24 @@ class ListScreenViewModel @Inject constructor(
     articlesRepository: ArticlesRepository
 ) : ViewModel() {
 
+    enum class AvailableCategories(val title: String) {
+        General("General"),
+        Business("Business"),
+        Entertainment("Entertainment"),
+        Health("Health"),
+        Science("Science"),
+        Sports("Sports"),
+        Technology("Technology")
+    }
+
+    var selectedCategory by mutableStateOf(AvailableCategories.General)
+        private set
+
     val topArticles = articlesRepository.getTopArticlesPagingData(
         pageSize = 30
     ).cachedIn(viewModelScope)
+
+    fun selectCategory(category: AvailableCategories) {
+        selectedCategory = category
+    }
 }
